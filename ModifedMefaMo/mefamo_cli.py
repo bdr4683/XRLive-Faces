3a734bbe774cf0397bbf4887150af68ae21930c5
tv4468 <tv4468@rit.edu>
2026-05-01 14:38:22 -0400

    mefamo doc

--- ModifedMefaMo/mefamo_cli.py
+++ ModifedMefaMo/mefamo_cli.py
@@ -4,23 +4,23 @@ from argparse import ArgumentParser
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('--input', default='0',
-                        help='Video source. Can be an integer for webcam or a string for a video file or directory. This is a directory that exists, it performs batch processing and sends results to an "out" directory in the parent directory.')
+                        help='Media source. Can be an integer for webcam or a string for a video/image file or directory. In case of directory, enables performance-focused batch processing and outputs within parent directory.')
     parser.add_argument('--ip', default='127.0.0.1',
                         help='IP address of the Unreal LiveLink server.')
     parser.add_argument('--port', default=11111,
                         help='Port of the Unreal LiveLink server.')
     parser.add_argument('--show_3d', action='store_true',
-                        help='Show the 3d face image (projected into a 2d window')
+                        help='Linux only: Show the 3d face image (projected into a 2d window)')
     parser.add_argument('--hide_image', action='store_true',
                         help='Hide the image window.')
     parser.add_argument('--show_debug', action='store_true',
                         help='Show debug window.')
-    parser.add_argument('--no_noise', action='store_true',
-                        help='Disables overlaying blendshape values perlin noise.')
+    parser.add_argument('--add_noise', action='store_true',
+                        help='Enable overlaying blendshape values perlin noise.')
     parser.add_argument('--calibrate', action='store_true',
                         help='Use alternate ranges when calculating blendshapes based on the first frame of (not recommended for images).')
     args = parser.parse_args()
 
     print("Starting MeFaMo")
-    mediapipe_face = Mefamo(args.input, args.ip, args.port, args.show_3d, args.hide_image, args.show_debug, args.no_noise, args.calibrate)
+    mediapipe_face = Mefamo(args.input, args.ip, args.port, args.show_3d, args.hide_image, args.show_debug, args.add_noise, args.calibrate)
     mediapipe_face.start()
--- ModifedMefaMo/setup.py
+++ ModifedMefaMo/setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.19',
     description='Python Library for generating blendshapes with MediaPipe FaceMesh and sending it to the Unreal Engine for live face tracking',
     author='Marco Pattke, Thomas Vilar',
-    author_email='j1m_w3st@web.de',
+    author_email='tv4468@rit.edu',
     packages=find_packages(),
     install_requires=[
         'numpy',
